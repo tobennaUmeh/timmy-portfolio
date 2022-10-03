14a5e1d1435136c85270b69c4dc204f38390ec2d
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2022-10-04 00:11:19 +0100

    initial setup complete

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import WlcmBG from '../Assets/wlcmBg.svg';
+import './Welcome.css';
+
+const WelcomePage = () => {
+	return <div className=''></div>;
+};
+
+export default WelcomePage;
---
+++ src/pages/tutoringPage/TutoringPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TutoringPage = () => {
+  return (
+    <div>TutoringPage</div>
+  )
+}
+
+export default TutoringPage
