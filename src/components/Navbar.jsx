b0755572d20729ccfadde48a617b14c333e32c83
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2022-10-26 00:21:45 +0100

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,21 @@ import Insta from '../Assets/instagram.svg';
 import Linkedin from '../Assets/linkedin.svg';
 import Git from '../Assets/github.svg';
 import Twitter from '../Assets/twitter.svg';
+import hamburger from '../Assets/hamburger.svg';
+import { useEffect } from 'react';
 
 const Navbar = () => {
+	useEffect(() => {
+		const togglebtn = document.querySelector('.toggle-button');
+		const navLinks = document.querySelector('.navbarEl');
+
+		togglebtn.addEventListener('click', () => {
+			return navLinks.classList.toggle('collapse');
+		});
+	}, []);
+
+	// console.log(navLinks);
+
 	const navEl = [
 		{ text: 'About Me', link: '/', key: 1 },
 		{ text: 'Design', link: '/' },
@@ -28,16 +41,15 @@ const Navbar = () => {
 			<NavLink to='/home'>
 				<img src={navIcon} className='navbarBrand' alt='Icon' />
 			</NavLink>
-			<div className='navbarEl'>
+			<img src={hamburger} alt='hamburger' className='toggle-button' />
+			<div className='navbarEl collapse'>
 				<ul className='nav-Links'>
 					{navEl.map((El) => (
-						<NavLink
-							key={El.text}
-							className='nav-link'
-							to={El.link}
-						>
-							{El.text}
-						</NavLink>
+						<li key={El.text}>
+							<NavLink className='nav-link' to={El.link}>
+								{El.text}
+							</NavLink>
+						</li>
 					))}
 				</ul>
 			</div>
